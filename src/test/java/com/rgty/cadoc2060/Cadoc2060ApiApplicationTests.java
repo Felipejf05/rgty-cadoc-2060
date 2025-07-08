df8804598553f1be6636965d74cc533ccd52e66e
FelipeJf05 <jorgefelipe.jf01@gmail.com>
2025-07-08 12:30:51 -0300

    First commit

---
+++ src/test/java/com/rgty/cadoc2060/Cadoc2060ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgty.cadoc2060;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cadoc2060ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
